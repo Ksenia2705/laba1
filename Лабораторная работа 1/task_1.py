136212a686ffb8b32cefae931f80dc1910d10d3b
Ksenia2705 <kchepkalenko@gmail.com>
2024-10-22 22:55:22 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,9 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
+index = numbers.index(None)
 dlina = len(numbers)
-summa = sum(numbers[:4]) + sum(numbers[5:])
+summa = sum(numbers[:index]) + sum(numbers[(index+1):])
 new = summa / dlina
-numbers[4] = new
+numbers[index] = new
 
 print("Измененный список:", numbers)
