b22be47b9acfcb20466224003d049efa52c1cbfd
Ksenia2705 <kchepkalenko@gmail.com>
2024-10-22 22:57:52 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,6 +1,6 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-V = 1.44
+volume_on_disk = 1.44
 pages = 100
 st_on_page = 50
 k_slo_in_st = 25
@@ -9,6 +9,6 @@ v_book_byte = 4 * k_slo_in_st * st_on_page * pages
 
 v_book_Mb = v_book_byte/(1024**2)
 
-count = int(V//v_book_Mb)
+count = round(volume_on_disk/v_book_Mb)
 
 print("Количество книг, помещающихся на дискету:", count)
